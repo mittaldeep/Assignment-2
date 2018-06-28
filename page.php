677b01b97466321a946e03540465c198637bd05b
Deepanshu Mittal <mittaldeepanshu404@gmail.com>
2018-06-28 12:34:43 +0530

    first assignment

---
+++ page.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>The LoL Engineers</title>
+<style type="text/css">
+.header{
+        width:100%;
+		height:200px;
+		border: dotted;
+		
+		background-color: #CCFFFF;
+		float:left;
+		text-align:center;
+		font-size:40px;
+	
+		}
+.menu{
+       width:100%;
+		height:100px;
+		border: dotted;
+		
+		background-color:#CCFF00;
+		float:left;
+		text-align:center;
+		font-size:30px;
+		}
+		.image{
+   			 width:100%;
+		height: 200px;
+		border: dotted;
+		
+		background-color: #FF33FF;
+		float:left;
+		text-align: left;
+		font-size: 10px;
+		}
+		.footer{
+        width:100%;
+		height:100px;
+		border: dotted;
+		
+		background-color: #9933FF;
+		float:left;
+		text-align:center;
+		font-size:40px;
+	
+		}
+		</style>
+		</head>
+
+<body>
+<div class="header">
+<img src="download.jpg" height="90px" align="left" style="width:20%;">
+<img src="5.jpg" height="150px" align="texttop" style="width:20%;">
+The LoL Engineers
+
+
+</div>
+<div class="menu">
+<h2 >"Team One Dream One"</h2>
+</div>
+<div class="image">
+
+<img src="contact.jpg" height="200px" align="left" style="width:100%;">
+</div>
+<div class="footer">
+<br />
+
+Copyright@The LoL Engineers
+
+
+</div>
+
+</body>
+</html>
+
+    
